735f5d43fccb82b880ad184f3de321e5bd371813
Soren Hansen <Soren.Hansen@ril.com>
2015-01-15 00:05:36 +0530

    Force ssl compression always
    Disabling ssl compression causes the SSL connection to follow an
    entirely different code path, which simply does not work in our
    environment. It's unclear what's causing it, but eventlet's read()
    method on the SSL connection object throws a SysCallError exception with
    errno 9 (Invalid File Descriptor). I've not been able to work out why
    that is, so pushing this patch as a temporary measure to work around it.

--- glanceclient/common/http.py
+++ glanceclient/common/http.py
@@ -67,7 +67,7 @@ class HTTPClient(object):
         self.timeout = float(kwargs.get('timeout', 600))
 
         if self.endpoint.startswith("https"):
-            compression = kwargs.get('ssl_compression', True)
+            compression = True
 
             if not compression:
                 self.session.mount("glance+https://", https.HTTPSAdapter())
